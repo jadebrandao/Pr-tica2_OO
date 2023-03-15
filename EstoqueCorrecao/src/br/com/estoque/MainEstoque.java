6618df14f67031463d4d6d0c995e2fc76b860769
Jade Brandão <108814737+jadebrandao@users.noreply.github.com>
2023-03-15 18:36:34 -0300

    Update MainEstoque.java

--- EstoqueCorrecao/src/br/com/estoque/MainEstoque.java
+++ EstoqueCorrecao/src/br/com/estoque/MainEstoque.java
@@ -10,6 +10,12 @@ public class MainEstoque {
         e1.setQtaAtual(10);
         e1.setQtaMinima(5);
         System.out.println(e1.mostra());
+        
+         Estoque E2 = new Estoque(1,"Caderno",20,1);
+        E2.darBaixa(10);
+        System.out.println("Estoque Atual: "+ E2.getQtaAtual());
+        E2.darBaixa(100);
+        System.out.println(E2.precisaRepor());
     }
 
 }
